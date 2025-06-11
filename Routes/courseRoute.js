ff1e917352e59a3aed6ae238520b9c7ae28216ff
Sulayman <developer.sulayman@gmail.com>
2025-06-11 22:03:03 +0600

    neccessary routes added from the courseRoutes

--- Routes/courseRoute.js
+++ Routes/courseRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createCourse, getCourses, getCoursesByUser, getIndividualCourse, getLatestCourses, getPopularCourses,  myEnrollments, removeEnrollment, toggleEnrollment } from '../Controllers/CourseController.js';
+import { createCourse, getCourses, getCoursesByUser, getIndividualCourse, getLatestCourses, getPopularCourses,  myEnrollments, removeCourse, removeEnrollment, toggleEnrollment, updateCourse } from '../Controllers/CourseController.js';
 import verifyToken from '../Middlewares/verityToken.js';
 import AuthCheck from '../Middlewares/AuthCheck.js';
 
@@ -11,9 +11,10 @@ router.patch('/courses/enroll/:courseId/email', verifyToken,AuthCheck, toggleEnr
 router.get('/myEnrollments/email', verifyToken,AuthCheck, myEnrollments);
 router.get('/mycourses/email',verifyToken,AuthCheck, getCoursesByUser)
 router.patch('/unenroll/:courseId/email', verifyToken,AuthCheck, removeEnrollment);
+router.delete('/course/:courseId/email', verifyToken,AuthCheck, removeCourse);
+router.put('/updatecourse/:courseId/email', verifyToken,AuthCheck, updateCourse); 
 
-
-router.get('/courses', verifyToken, getCourses);
+router.get('/courses',getCourses);
 router.get('/latest-courses',getLatestCourses);
 router.get('/courses/popular', getPopularCourses);
 router.get('/course/:courseId', getIndividualCourse);

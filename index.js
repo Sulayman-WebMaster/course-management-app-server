28a90d2718d6c0eaadcfac0d70832b29a6b88836
Sulayman <developer.sulayman@gmail.com>
2025-07-01 09:40:06 +0600

    cors changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ connectDB()
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin:process.env.CLIENT_URL,
+    origin:[process.env.CLIENT_URL, 'http://localhost:5173'],
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'] 
